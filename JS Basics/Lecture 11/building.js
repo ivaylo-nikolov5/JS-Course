c2e54ae3ced91649fa70f0b53f2d7db1ea9e745c
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-08-08 13:53:54 +0300

    Added a new solution to a problem

--- JS Basics/Lecture 11/building.js
+++ JS Basics/Lecture 11/building.js
@@ -1,4 +1,22 @@
 function building(input) {
     let floors = Number(input[0]);
     let roomsPerFloor = Number(input[1]);
+
+    for (let floor = floors; floor > 0; floor--) {
+        let currentFloor = "";
+        for (let room = 0; room < roomsPerFloor; room++) {
+            if (floor === floors) {
+                currentFloor += `L${floor}${room} `;
+            } else if (floor % 2 == 0) {
+                currentFloor += `O${floor}${room} `;
+            } else {
+                currentFloor += `A${floor}${room} `;
+            }
+
+        };
+
+        console.log(currentFloor);
+    }
 };
+
+building(["9", "5"])
