31b339477fb40e59a9e261ddeb301e4f9c957d35
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-02 12:14:46 +0200

    Added a new solution for the Right Place problem

--- JS Fundamentals/Lecture-3/rightPlace.js
+++ JS Fundamentals/Lecture-3/rightPlace.js
@@ -1,19 +1,23 @@
 function rightPlace(firstString, char, secondString) {
-    let newString = "";
+    // let newString = "";
 
-    for (let i = 0; i < firstString.length; i++) {
-        if (firstString[i] === "_") {
-            newString += char;
-        } else {
-            newString += firstString[i];
-        }
-    }
+    // for (let i = 0; i < firstString.length; i++) {
+    //     if (firstString[i] === "_") {
+    //         newString += char;
+    //     } else {
+    //         newString += firstString[i];
+    //     }
+    // }
 
-    if (newString === secondString) {
-        console.log("Matched");
-    } else {
-        console.log("Not Matched");
-    };
+    // if (newString === secondString) {
+    //     console.log("Matched");
+    // } else {
+    //     console.log("Not Matched");
+    // };
+
+    let result = firstString.replace("_", char);
+    let output = result === secondString ? "Matched" : "Not Matched";
+    console.log(output);
 
 }
 
