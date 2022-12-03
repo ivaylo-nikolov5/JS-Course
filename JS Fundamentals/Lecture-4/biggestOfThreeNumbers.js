5b23ca90ca93680a3cf61c4497633e926f45e35f
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-03 11:28:45 +0200

    Added a solution for the Biggest of 3 Numbers problem

---
+++ JS Fundamentals/Lecture-4/biggestOfThreeNumbers.js
@@ -0,0 +1,10 @@
+
+function biggestOfThreeNumbers(num1, num2, num3) {
+    let biggestNumber = Math.max(num1, num2, num3);
+    console.log(biggestNumber);
+}
+
+biggestOfThreeNumbers(-2,
+    7,
+    3
+)
