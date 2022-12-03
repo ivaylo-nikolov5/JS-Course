a5b2b3b4b36aaa60668f5d50766e2fe762260580
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-03 12:44:16 +0200

    Added a solution for the Triangle Area problem

---
+++ JS Fundamentals/Lecture-4/triangleArea.js
@@ -0,0 +1,10 @@
+function triangleArea(side1, side2, side3) {
+    let s = (side1 + side2 + side3) / 2;
+    let area = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    console.log(area);
+}
+
+triangleArea(2,
+    3.5,
+    4
+)
