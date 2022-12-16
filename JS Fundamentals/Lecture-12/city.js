fb41a6c767665291f2f5c4c7a6fe7b3715b52436
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-16 17:20:08 +0200

    Added a solution for the 02. City problem

--- JS Fundamentals/Lecture-12/city.js
+++ JS Fundamentals/Lecture-12/city.js
@@ -1,9 +1,7 @@
 function city(object) {
-    console.log(`name -> ${object.name}`);
-    console.log(`area -> ${object.area}`);
-    console.log(`population -> ${object.population}`);
-    console.log(`country -> ${object.country}`);
-    console.log(`postCode -> ${object.postCode}`);
+    for (let key of Object.keys(object)) {
+        console.log(`${key} -> ${object[key]}`);
+    }
 }
 
 city({
