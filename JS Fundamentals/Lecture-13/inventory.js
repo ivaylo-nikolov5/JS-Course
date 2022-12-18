a045c8e504b4e006ad2afb1f57e20241cf315005
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-18 11:03:21 +0200

    Added a solution for the 05. Inventory problem

--- JS Fundamentals/Lecture-13/inventory.js
+++ JS Fundamentals/Lecture-13/inventory.js
@@ -1,3 +1,37 @@
 function inventory(array) {
+    let heroes = [];
 
+    while (array.length > 0) {
+        let info = array.shift().split(" / ");
+        let name = info[0];
+        let level = Number(info[1]);
+        let items = info[2].split(", ");
+
+        let hero = {
+            name,
+            level,
+            items,
+        }
+
+        heroes.push(hero);
+    }
+
+    heroes = heroes.sort((a, b) => {
+        return a.level - b.level;
+    })
+
+    while (heroes.length > 0) {
+        let hero = heroes.shift();
+
+        console.log(`Hero: ${hero.name}`);
+        console.log(`level => ${hero.level}`);
+        console.log(`items => ${hero.items.join(", ")}`);
+    }
 }
+
+inventory([
+        'Isacc / 25 / Apple, GravityGun',
+        'Derek / 12 / BarrelVest, DestructionSword',
+        'Hes / 1 / Desolator, Sentinel, Antara'
+    ]
+)
