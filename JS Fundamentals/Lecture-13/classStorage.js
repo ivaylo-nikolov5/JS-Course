dcd3295b457f54e7c66995553626da3ec11770be
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-18 16:35:04 +0200

    Added a solution for the 08. Class Storage problem

---
+++ JS Fundamentals/Lecture-13/classStorage.js
@@ -0,0 +1,29 @@
+class Storage {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.storage = [];
+        this.totalCost = 0;
+    }
+
+    addProduct(product) {
+        this.storage.push(product);
+        this.capacity -= product.quantity;
+        this.totalCost += product.price * product.quantity;
+    }
+
+    getProducts() {
+        let result = [];
+        for (let i = 0; i < this.storage.length; i++) {
+            result.push(JSON.stringify(this.storage[i]));
+        }
+
+        return result.join("\n");
+    }
+}
+
+let productOne = {name: 'Tomato', price: 0.90, quantity: 19};
+let productTwo = {name: 'Potato', price: 1.10, quantity: 10};
+let storage = new Storage(30);
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+console.log(storage.totalCost);
