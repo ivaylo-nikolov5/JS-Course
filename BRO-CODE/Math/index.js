fd1f5d3d466e05d8a1014cc56c00b302e911fc67
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-20 19:30:34 +0200

    Math part

---
+++ BRO-CODE/Math/index.js
@@ -0,0 +1,16 @@
+let x = 3.14;
+
+//x = Math.round(x);
+//x = Math.floor(x);
+//x = Math.ceil(x);
+//x = Math.pow(x, 5);
+//x = Math.abs(-3.14);
+//x = Math.sqrt(9);
+//x = Math.log(x);
+//x = Math.max(1, 2, x);
+//x = Math.min(1, 2, 3, 4, 5, 6);
+x = Math.random();
+
+const pi = Math.PI;
+
+console.log(x);
