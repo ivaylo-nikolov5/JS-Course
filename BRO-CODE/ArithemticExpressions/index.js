4f03cb76c0fddfd6805badcee5c3cd8599deb0c4
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-20 12:51:42 +0200

    Arithmetic Operations part

---
+++ BRO-CODE/ArithemticExpressions/index.js
@@ -0,0 +1,23 @@
+let students = 20;
+
+// addition
+students = students + 1;
+// students += 1;
+
+// subtraction
+students = students - 1;
+// students -= 1;
+
+// multiplication
+students = students * 2;
+// students *= 2;
+
+// division
+students = students / 2;
+// students /= 2;
+
+// modular division
+students = students % 3;
+// students %= 3;
+
+console.log(students);
