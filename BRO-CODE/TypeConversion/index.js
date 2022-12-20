28ac86a8873fdeae2a9893c2e6cc32f964c397ab
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-20 14:31:49 +0200

    Type Conversion part

---
+++ BRO-CODE/TypeConversion/index.js
@@ -0,0 +1,13 @@
+let age = window.prompt("How old are you?");
+age = Number(age) + 1;
+
+console.log(`Happy birthday! You are ${age} years old!`);
+
+
+let x = Number("3.14");
+let y = String(3.14);
+let z = Boolean("");
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
