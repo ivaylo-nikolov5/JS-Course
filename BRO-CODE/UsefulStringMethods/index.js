5a48e3f05a0d1843612a12eba8e19e3d26f7de37
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-26 09:41:17 +0200

    Useful string methods part

--- BRO-CODE/UsefulStringMethods/index.js
+++ BRO-CODE/UsefulStringMethods/index.js
@@ -1,7 +1,18 @@
-let username = "Ivaylo Nikolov";
+let username = " Ivaylo Nikolov ";
+let phoneNumber = "+359-89-993-1234";
 
 let result;
 
 result = username.length;
+result = username.charAt(3);
+result = username.charCodeAt(3);
+result = username.indexOf("y");
+result = username.lastIndexOf("o");
+result = username.endsWith("v");
+result = username.trim();
+result = username.toUpperCase();
+result = username.toLowerCase();
 
-console.log(result);
+phoneNumber = phoneNumber.replaceAll("-", ".");
+
+console.log(phoneNumber);
