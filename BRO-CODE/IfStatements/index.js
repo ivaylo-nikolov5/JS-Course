0d7e5e3eb5dbeb95a8c1ea107b8a3a03b7c55140
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-26 17:38:03 +0200

    If statements part

---
+++ BRO-CODE/IfStatements/index.js
@@ -0,0 +1,22 @@
+let age = 18;
+let result;
+
+/*
+if (age > 18) {
+    result = "You are already an adult";
+} else if (age === 18) {
+    result = "You just became an adult";
+} else {
+    result = "You are still not an adult";
+}
+
+console.log(result);
+*/
+
+let online = true;
+
+if (online) {
+    console.log("You are online!");
+} else {
+    console.log("You are offline!");
+}
