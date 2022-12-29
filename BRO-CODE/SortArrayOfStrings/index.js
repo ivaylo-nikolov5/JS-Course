ec3c4184f5a84256f23e04f718d59ac7b7839337
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2022-12-29 19:08:25 +0200

    Sort an array of strings part

---
+++ BRO-CODE/SortArrayOfStrings/index.js
@@ -0,0 +1,8 @@
+let fruits = ["banana", "orange", "apple", "kiwi", "strawberry", "blueberry"];
+
+//fruits = fruits.sort();
+fruits = fruits.sort().reverse();
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
