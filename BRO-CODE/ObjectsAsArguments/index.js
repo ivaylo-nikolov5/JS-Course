d6469c83dcd5e9976ece49369c3e7cbac71e2444
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-01 23:07:00 +0200

    Objects as arguments part

---
+++ BRO-CODE/ObjectsAsArguments/index.js
@@ -0,0 +1,27 @@
+class Car {
+    constructor(model, year, color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+}
+
+const car1 = new Car("Mustang", 2023, "red");
+const car2 = new Car("Corvette", 2024, "blue");
+const car3 = new Car("Lambo", 2022, "yellow");
+
+changeColor(car3, "green")
+
+displayInfo(car1);
+displayInfo(car2);
+displayInfo(car3);
+
+function displayInfo(car) {
+    console.log(car.model);
+    console.log(car.year);
+    console.log(car.color);
+}
+
+function changeColor(car, newColor) {
+    car.color = newColor;
+}
