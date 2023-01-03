c33f536ac0f5936ed6747a5813ccbcc895831c3e
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-03 13:43:52 +0200

    ES6 modules part

---
+++ BRO-CODE/ES6Modules/index.js
@@ -0,0 +1,6 @@
+import { PI, trianglePerimeter, squarePerimeter } from "./math_util.js";
+import * as MathUtil from "./math_util.js";
+
+console.log(PI);
+console.log(trianglePerimeter(1, 5, 7));
+console.log(squarePerimeter(4));
---
+++ BRO-CODE/ES6Modules/math_util.js
@@ -0,0 +1,4 @@
+export const PI = 4.14159;
+
+export const trianglePerimeter = (a, b, c) => a + b + c;
+export const squarePerimeter = (a) => a * 4;
