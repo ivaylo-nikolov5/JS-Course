fe0f29b0be369e40e71b61ecee185dc4e7bdf29d
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-03 18:52:35 +0200

    Add/Change CSS properties part

---
+++ BRO-CODE/Add/ChangeCSSProperties/index.js
@@ -0,0 +1,10 @@
+const title = document.getElementById("myTitle");
+
+
+//title.style.backgroundColor = "lightgreen";
+// title.style.backgroundColor = "rgb(10, 102, 0";
+title.style.backgroundColor = "#222222";
+title.style.color = "rgb(50, 200, 250)";
+title.style.fontFamily = "consolas";
+title.style.textAlign = "center";
+title.style.border = "2px solid"
