3718a0a0e169ed72770d5fa1395b6dde453bb54c
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-06 12:54:25 +0200

    Window part

---
+++ BRO-CODE/Window/index.js
@@ -0,0 +1,22 @@
+//console.dir(window);
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+// console.log(window.outerWidth);
+// console.log(window.outerHeight);
+// console.log(window.scrollX);
+// console.log(window.scrollY);
+// window.location.href = "https://google.com";
+// console.log(window.location.href);
+// console.log(window.location.hostname);
+// console.log(window.location.pathname);
+
+//const myButton = document.querySelector("#myButton");
+// myButton.addEventListener("click", () => window.open("https://google.com"))
+//closes the current window(I suggest u to better don't click it)
+// myButton.addEventListener("click", () => window.close());
+// myButton.addEventListener("click", () => window.print());
+
+// window.alert("HELLO!");
+// window.confirm("Press OK to continue!");
+
+const name = window.prompt("Type your name here!");
