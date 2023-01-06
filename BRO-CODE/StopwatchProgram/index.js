fce69ccd22f0ee329fd973b05932d24ba8274d92
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-06 22:27:34 +0200

    Stopwatch part

--- BRO-CODE/StopwatchProgram/index.js
+++ BRO-CODE/StopwatchProgram/index.js
@@ -3,27 +3,58 @@ const startBtn = document.querySelector("#startBtn");
 const pauseBtn = document.querySelector("#pauseBtn");
 const stopBtn = document.querySelector("#stopBtn");
 
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let paused = true;
+let intervalId;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
 
-startBtn.addEventListener("click", setInterval(startTimer, 1000));
-pauseBtn.addEventListener("click", pauseTimer);
-stopBtn.addEventListener("click", stopTimer);
+startBtn.addEventListener("click", () => {
+    if (paused) {
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(updateTime, 1000)
+    }
+});
+pauseBtn.addEventListener("click", () => {
+    if (!paused) {
+        paused = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalId);
+    }
+});
+stopBtn.addEventListener("click", () => {
+    let paused = true;
+    clearInterval(intervalId);
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    display.textContent = `00:00:00`;
 
-function startTimer() {
-    seconds += 1
-    seconds %= 60;
-    console.log(seconds);
-}
+});
 
+function updateTime() {
+    elapsedTime = Date.now() - startTime;
+    secs = Math.floor(elapsedTime / 1000 % 60);
+    mins = Math.floor(elapsedTime / (1000 * 60) % 60);
+    hrs = Math.floor(elapsedTime / (1000 * 60 * 60) % 60);
 
-function pauseTimer() {
+    secs = formatTime(secs);
+    mins = formatTime(mins);
+    hrs = formatTime(hrs);
+    mins
+    display.textContent = `${hrs}:${mins}:${secs}`;
 
-}
-
-function stopTimer() {
-
+    function formatTime(time) {
+        return (("0") + time).length > 2 ? time: "0" + time;
+    }
+    
 }
 
 
