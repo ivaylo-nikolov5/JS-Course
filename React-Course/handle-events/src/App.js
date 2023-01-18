e9e17204c794ffe9d42b7b7d9918bc38fcf46f40
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-18 12:51:37 +0200

    Handle events

---
+++ React-Course/handle-events/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ToggleButton from "./ToggleButton"
+
+function App() {
+  return <div>
+      <ToggleButton />
+  </div>
+}
+
+export default App;
