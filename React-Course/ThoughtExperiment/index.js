1c7a4073c4d71587406891c6bab3de54c4e9e157
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-03 14:17:59 +0200

    Thought experiment part

---
+++ React-Course/ThoughtExperiment/index.js
@@ -0,0 +1,16 @@
+const page = (
+    <div>
+        <h1>Who is Andrew Tate</h1>
+        <h3>Reasons I like Andrew Tate: </h3>
+
+        <ul>
+            <li>He tells the truth</li>
+            <li>He is breaking the matrix</li>
+            <li>He is the TOP G</li>
+            <li>He is teaching us how to become a TOP G</li>
+            <li>He has 5 m. dollars Bugatti</li>
+        </ul>
+    </div>
+)
+ReactDOM.render(page, document.getElementById("root"))
+// document.getElementById("root").append(JSON.stringify(page));
