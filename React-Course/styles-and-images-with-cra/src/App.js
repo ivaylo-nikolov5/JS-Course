508fd2ef30793f70b2d3eea6ff659be208b90a76
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-08 16:19:21 +0200

    Added the main structure of the project

---
+++ React-Course/styles-and-images-with-cra/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import "./style.css"
+
+
+function App() {
+    return (
+        <div className="containers">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+
+export default App;
