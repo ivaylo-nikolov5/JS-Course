1599d5ada0095a4d96f1c58b3f8943a5bc69cc7c
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-09 11:16:12 +0200

    Added main section and styled it

--- React-Course/styles-and-images-with-cra/src/components/Main.js
+++ React-Course/styles-and-images-with-cra/src/components/Main.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "../main-section.css";
 
 function Main() {
-    return <h1>Main component</h1>
+    return (
+    <div>
+        <div className="title-section">
+            <h1 className="title">Fun facts about React</h1>
+        </div>
+
+        <div className="list-section">
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers housands of enterprice apps, including mobile apps</li>
+            </ul>
+        </div>
+    </div>
+    );
 }
 
 export default Main;
