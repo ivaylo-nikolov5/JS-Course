b5f7dc625150669ca5bb3bb81f37c41ae374eb11
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-10 15:42:53 +0200

    Added and styled the second section

--- React-Course/airbnb-experiences-clone/src/components/SecondSection.js
+++ React-Course/airbnb-experiences-clone/src/components/SecondSection.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "../second-section.css";
+import photoGrid from "../images/photo-grid.png";
 
 function SecondSection() {
     return (
         <div className="second-section">
+            <div className="image-section">
+                <img src={photoGrid} className="photo-grid"/>
+            </div>
 
+            <div className="second-section-info">
+                <h1 className="second-section-title">Online Experiences</h1>
+                <p className="second-section-text">
+                    Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.
+                </p>
+            </div>
         </div>
     )
 }
