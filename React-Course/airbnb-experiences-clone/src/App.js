dbf865ac97715260f5e6a2b8d6a042c32aeb1016
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-12 11:33:10 +0200

    Added props to the card component

--- React-Course/airbnb-experiences-clone/src/App.js
+++ React-Course/airbnb-experiences-clone/src/App.js
@@ -3,13 +3,20 @@ import Card from "./components/Card";
 import Navbar from "./components/Navbar";
 import SecondSection from "./components/SecondSection";
 import "./style.css";
+import katieZaferes from "./images/katie-zaferes.png";
 
 function App() {
     return (
         <div>
             <Navbar />
             <SecondSection />
-            <Card />
+            <Card 
+                img={katieZaferes}
+                rating={"5.0"}
+                reviewCount={6}
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
         </div>
     )
 }
--- React-Course/airbnb-experiences-clone/src/components/Card.js
+++ React-Course/airbnb-experiences-clone/src/components/Card.js
@@ -1,27 +1,26 @@
 import React from "react";
 import "../card.css"
-import katieZaferes from "../images/katie-zaferes.png";
 import star from "../images/star.png";
  
 function Card(props) {
     return (
         <div className="card-container">
-            <div className="card-condition">
+            {/* <div className="card-condition">
                 SOLD OUT
-            </div>
+            </div> */}
 
-            <img src={katieZaferes} className="card-photo"/>
+            <img src={props.img} className="card-photo"/>
 
             <p className="card-reviews">
                 <img src={star} className="orange-star"/>
                 <span className="reviews-text">
-                    5.0 <span className="grayed-text">(6)&#183;USA</span>
+                    {props.rating} <span className="grayed-text">({props.reviewCount})&#183;{props.country}</span>
                 </span>
             </p>
 
-            <p className="card-description">Life lessons with Katie Zaferas</p>
+            <p className="card-description">{props.title}</p>
 
-            <p className="card-price">From $136 <span className="price-per-person">
+            <p className="card-price">From ${props.price} <span className="price-per-person">
                 <span className="slash">/
                 </span> person</span></p>
         </div>
