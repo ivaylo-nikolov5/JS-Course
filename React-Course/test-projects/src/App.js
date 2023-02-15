61c36cf1b900c53f396c6e4cd91a0814a44952b0
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-15 17:06:24 +0200

    Practiced state

--- React-Course/test-projects/src/App.js
+++ React-Course/test-projects/src/App.js
@@ -2,25 +2,27 @@ import React from "react";
 import './style.css';
 
 function App() {
-    const [counter, setCounterValue] = React.useState(0)
+    const [count, setCount] = React.useState(0)
 
-    function incrementCounter() {
-        setCounterValue(counter + 1);
+    function add() {
+        console.log("add");
+        setCount(prevCount => prevCount + 1);
     }
     
-    function decrementCounter() {
-        setCounterValue(counter - 1);
+    function sub() {
+        console.log("subtract");
+        setCount(prevCount => prevCount - 1);
     }    
 
     return (
         <div className="counter">
             <div className="counter--count">
                 <button className="counter--minus" 
-                    onClick={incrementCounter}
+                    onClick={sub}
                 >-</button>
-                <h1>0</h1>
+            <h1>{count}</h1>
                 <button className="counter--plus" 
-                    onClick={decrementCounter}
+                    onClick={add}
                 >+</button>
             </div>
         </div>
