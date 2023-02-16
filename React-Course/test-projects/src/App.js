2021939ec7d733ad2e5b80d1acdf9141b3da49cb
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-16 19:53:57 +0200

    Implemented toggle function

--- React-Course/test-projects/src/App.js
+++ React-Course/test-projects/src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
+import "./style.css"
 import boxes from "./boxes"
+import Box from "./Box";
 
 
-function App() {
+
+function App(props) {
+    const [boxesArray, setBoxesArray] = React.useState(boxes);
+    
+    function toggle(id) {
+        setBoxesArray(prevBoxes => {
+            const newBoxes = [];
+
+            for (let i = 0; i < prevBoxes.length; i++) {
+                const currentSquare = prevBoxes[i];
+                if (currentSquare.id === id) {
+                    const updatedSquare = {
+                        ...currentSquare,
+                        on: !currentSquare.on
+                    }
+                    newBoxes.push(updatedSquare);
+                } else {
+                    newBoxes.push(currentSquare);
+                }
+            }
+
+            return newBoxes;
+        })
+    }
+
+    const boxDivs = boxesArray.map(box => {
+        return <Box     
+            key={box.id}
+            on={box.on}
+            id={box.id}
+            toggle={toggle}
+        />
+    })
+
     return (
-        <div>
-            
+        <div className="mainContainer">
+            {boxDivs}
         </div>
     )
 }
---
+++ React-Course/test-projects/src/boxes.js
@@ -0,0 +1,67 @@
+let id = 1;
+
+
+let boxes = [
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    },
+    {
+        id: id++,
+        on: false
+    }    
+]
+
+export default boxes;
