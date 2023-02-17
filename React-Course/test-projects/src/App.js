0cc68950585cf01474cca3c5e981c2b493546074
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-17 20:25:16 +0200

    Conditional rendering challenge

--- React-Course/test-projects/src/App.js
+++ React-Course/test-projects/src/App.js
@@ -4,9 +4,12 @@ import "./style.css"
 function App() {
     const [messages, setMessages] = React.useState(["a", "b"])
 
+    const unreadMessages = messages.length === 0 ? "You're all caught" : 
+    `You have ${messages.length} unread ${messages.length === 1 ? "message" : "messages"}`;
+
     return (
         <div>
-            {messages.length > 0 && <h1>You have {messages.length} unread messages!</h1>}
+            <h1>{unreadMessages}</h1>
         </div>
     )
 }
