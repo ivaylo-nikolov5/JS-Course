0ec5986ee1bbce1575cef1bb0782418b4de1d1e7
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-18 16:53:16 +0200

    Added functionality to the form

--- React-Course/test-projects/src/App.js
+++ React-Course/test-projects/src/App.js
@@ -2,32 +2,88 @@ import React from "react";
 import "./style.css"
 
 function App() {
+    const [formData, setFormData] = React.useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
+        newsletterSubscription: false
+    })
+
+    function handleChange(event) {
+        setFormData(prevFormData => {
+            const {name, value, type, checked} = event.target;
+            return {
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value 
+            }
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        if (formData.password === "" 
+            || formData.confirmPassword === "" 
+            || formData.email === "") {
+            console.log("There cannot be empty fields!");
+            return;
+        }         
+        
+
+
+        if (formData.password !== formData.confirmPassword) {
+            console.log("Passwords do not match!");
+            return;
+        } 
+        
+
+        else
+        console.log("Succesfully signed up!");
+
+        if (formData.newsletterSubscription) 
+        console.log("Thanks for signing up for our newsletter!");
+    }
+
     return (
         <div className="formContainer">
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
                 <input 
                     className="formTextBox"
-                    placeholder="Email"                
+                    placeholder="Email"
+                    onChange={handleChange}
+                    name="email"
+                    value={formData.email}                
                 />
 
                 <input 
+                    type="password"
                     className="formTextBox"
-                    placeholder="Password"                
+                    placeholder="Password" 
+                    onChange={handleChange}
+                    name="password"
+                    value={formData.password}                  
                 />
 
                 <input 
+                    type="password"
                     className="formTextBox"
-                    placeholder="Confirm password"                
+                    placeholder="Confirm password" 
+                    onChange={handleChange}
+                    name="confirmPassword"
+                    value={formData.confirmPassword}                  
                 />
 
                 <div className="newsletter-subsciption">
                     <input 
+                        id="newsletterSubscription"
                         type="checkbox" 
-                        name="newsletter"
-                        className="newsletter-checkbox"    
+                        name="newsletterSubscription"
+                        className="newsletter-checkbox"  
+                        onChange={handleChange}
+                        checked={formData.newsletterSubscription}    
                     />
                     <label 
-                        htmlFor="newsletter"
+                        htmlFor="newsletterSubscription"
                         className="newsletter-label">
                         I want to join the newsletter
                     </label>
