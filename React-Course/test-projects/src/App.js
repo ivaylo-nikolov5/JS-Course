17e0381fd93599cbfe920e99a75de5c1b9d6e780
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-20 18:26:40 +0200

    Api fetch challenge part

--- React-Course/test-projects/src/App.js
+++ React-Course/test-projects/src/App.js
@@ -3,15 +3,27 @@ import "./style.css"
 
 function App() {
     const [starWarsData, setStarWarsData] = React.useState({});
+    const [count, setCount] = React.useState(1);
 
-    // console.log("Component rendered")
+    console.log("Component rendered")
 
-    fetch("https://swapi.dev/api/people/1")
-        .then(res => res.json())
-        .then(data => setStarWarsData(data));
+    function handleClick() {
+        setCount(prevCount => prevCount + 1);
+    }
+
+
+    React.useEffect(function() {
+        fetch(`https://swapi.dev/api/people/${count}`)
+            .then(res => res.json())
+            .then(data => setStarWarsData(data));
+    }, [count])
 
     return (
         <div>
+            <h2>The count is {count}</h2>
+            <button
+                onClick={handleClick}
+            >Get Next Character</button>
             <pre>{JSON.stringify(starWarsData, null, 4)}</pre>
         </div>
     )
